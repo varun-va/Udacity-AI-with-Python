c3bba4f24d316125286c9b27b52c5a883287a4aa
varunsv <varunsv@users.noreply.github.com>
2018-08-06 22:07:20 -0700

    Output of results_dict

---
+++ results_dict_read.py
@@ -0,0 +1,46 @@
+results_dict = {
+'Golden_retriever_05257.jpg': ['golden retriever', 'golden retriever', 1, 1, 1],
+'German_shepherd_dog_04931.jpg': ['germanshepherd dog', 'german shepherd, german shepherd dog, german police dog, alsatian', 1, 1, 1], 'German_shepherd_dog_04890.jpg': ['german shepherd dog', 'german shepherd, german shepherd dog, german police dog, alsatian', 1, 1, 1],
+'cat_07.jpg': ['cat', 'egyptian cat', 1, 0, 0],
+'Golden_retriever_05195.jpg': ['golden retriever', 'golden retriever', 1, 1, 1],
+'Basset_hound_01034.jpg': ['basset hound', 'basset, basset hound', 1, 1, 1],
+'Poodle_07927.jpg': ['poodle', 'standard poodle', 1, 1, 1],
+'Boston_terrier_02259.jpg': ['boston terrier', 'boston bull, boston terrier', 1, 1, 1],
+'Miniature_schnauzer_06884.jpg': ['miniature schnauzer', 'miniature schnauzer', 1, 1, 1],
+'Saint_bernard_08010.jpg': ['saint bernard', 'saint bernard, st bernard', 1, 1, 1],
+'skunk_029.jpg': ['skunk', 'skunk, polecat, wood pussy', 1, 0, 0],
+'German_shorthaired_pointer_04986.jpg': ['german shorthaired pointer', 'german shorthaired pointer', 1, 1, 1],
+'Basenji_00963.jpg': ['basenji', 'basenji', 1, 1, 1],
+'Boston_terrier_02285.jpg': ['boston terrier', 'boston bull, boston terrier', 1, 1, 1],
+'Poodle_07956.jpg': ['poodle', 'standard poodle', 1, 1, 1],
+'Great_dane_05320.jpg': ['great dane', 'great dane', 1, 1, 1],
+'Dalmatian_04068.jpg': ['dalmatian', 'dalmatian,coach dog, carriage dog', 1, 1, 1],
+'Basenji_00974.jpg': ['basenji', 'basenji', 1, 1, 1],
+'Dalmatian_04017.jpg': ['dalmatian', 'dalmatian, coach dog, carriage dog', 1, 1, 1],
+'Saint_bernard_08036.jpg': ['saint bernard', 'saint bernard, st bernard', 1, 1, 1],
+'Rabbit_002.jpg': ['rabbit', 'wood rabbit, cottontail, cottontail rabbit', 1, 0, 0],
+'Golden_retriever_05223.jpg': ['golden retriever', 'golden retriever', 1, 1, 1],
+'Beagle_01141.jpg': ['beagle', 'beagle', 1, 1, 1], 'cat_02.jpg': ['cat', 'tabby, tabby cat', 1, 0, 0],
+'Boxer_02426.jpg': ['boxer', 'boxer', 1, 1, 1],
+'Beagle_01170.jpg': ['beagle', 'walker hound, walker foxhound', 0, 1, 1],
+'Dalmatian_04037.jpg': ['dalmatian', 'dalmatian, coach dog, carriage dog', 1, 1, 1],
+'Collie_03797.jpg': ['collie', 'collie', 1, 1, 1],
+'Beagle_01125.jpg': ['beagle', 'beagle', 1, 1, 1],
+'Golden_retriever_05182.jpg': ['golden retriever', 'golden retriever', 1, 1, 1],
+'Great_pyrenees_05435.jpg': ['great pyrenees', 'great pyrenees', 1, 1, 1],
+'fox_squirrel_01.jpg': ['fox squirrel', 'fox squirrel, eastern fox squirrel, sciurus niger', 1, 0, 0],
+'Great_pyrenees_05367.jpg': ['great pyrenees', 'kuvasz', 0, 1, 1],
+'great_horned_owl_02.jpg': ['great horned owl', 'ruffed grouse, partridge, bonasa umbellus', 0, 0, 0],
+'Boston_terrier_02303.jpg': ['boston terrier', 'boston bull, boston terrier', 1, 1, 1],
+'cat_01.jpg': ['cat', 'lynx, catamount', 0, 0, 0],
+'polar_bear_04.jpg': ['polar bear', 'ice bear, polar bear, ursus maritimus, thalarctos maritimus', 1, 0, 0],
+'gecko_80.jpg': ['gecko', 'tailed frog, bell toad, ribbed toad, tailed toad, ascaphus trui', 0, 0, 0],
+'Cocker_spaniel_03750.jpg': ['cocker spaniel',
+'cocker spaniel, english cocker spaniel, cocker', 1, 1, 1],
+'gecko_02.jpg': ['gecko','banded gecko', 1, 0, 0]
+}
+
+for key in results_dict:
+    if results_dict[key][3] == 1:
+        print(results_dict[key])
+
